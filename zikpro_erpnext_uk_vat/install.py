0c31f2018c764b25ae9e159cfbcc6471494a3b57
ali <ali@ubuntu-2204.linuxvmimages.local>
2026-03-15 21:09:13 -0400

    issue resolved install.py file

--- zikpro_erpnext_uk_vat/install.py
+++ zikpro_erpnext_uk_vat/install.py
@@ -1,4 +1,4 @@
-from your_app.patches.v1_0.add_eu_vat_fields import create_custom_fields
+from zikpro_erpnext_uk_vat.patches.v1_0.add_eu_vat_fields import create_custom_fields
 
 def after_install():
     create_custom_fields()
